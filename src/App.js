4038e77472aeb2ac492c226be7101684047ba5da
uutan <58uutan92@gmail.com>
2024-03-10 17:01:38 +0900

    add:タイトル画像追加

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
       context.rotate(startAngle + Math.PI / colors.length);
       context.textAlign = 'right';
       context.fillStyle = 'white';
-      context.font = '18px Arial';
+      context.font = '30px Arial';
       context.fillText(items[i], radius - 10, 0);
       context.strokeStyle = 'black';
       context.lineWidth = 0.3;
@@ -124,6 +124,7 @@ function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100vw', height: '100vh', maxWidth: '100%', margin: '0 auto', padding: '20px', overflow: 'hidden' }}>
+      <img src="image.png" alt="自分、なに飲むん？"/>
       <div style={{ position: 'relative', width: canvasSize.width, height: canvasSize.height, marginBottom: '20px' }}>
       <div style={{ width: '0', height: '0', borderLeft: '20px solid transparent', borderRight: '20px solid transparent', borderTop: '50px solid red', position: 'absolute', bottom: '93%', left: '50%', transform: 'translateX(-50%)', zIndex: '10' }}></div>
         <canvas ref={canvasRef} width={canvasSize.width} height={canvasSize.height} />

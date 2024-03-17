3744f4e78061d27aee68ce2cb2ffb7af4651fa6f
uutan <58uutan92@gmail.com>
2024-03-17 22:17:02 +0900

    add:ホームとチェックアウト画面に仮の画像を設定

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import backgroundImage from './images/home-background.jpg'; // 背景画像のパスをインポート
 
 function Home() {
   const navigate = useNavigate();
@@ -9,7 +10,15 @@ function Home() {
   };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{
+      textAlign: 'center',
+      marginTop: '50px',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover', // 背景画像をカバーするように設定
+      backgroundPosition: 'center', // 背景画像を中央に配置
+      width: '100vw',
+      height: '100vh' }}>
+
       <img src="image.png" alt="自分、なに飲むん？" style={{ maxWidth: '100%', marginBottom: '30px' }} />
       <p>居酒屋やバーで、どのお酒を飲むか悩んでいるそこのあなたへ</p>
       <button onClick={navigateToRoulette}>ルーレットに決めてもらう</button>

e8353908997ca56c52f49c8b659e820082287fe5
uutan <58uutan92@gmail.com>
2024-03-19 22:37:19 +0900

    add:READMEを追加

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import backgroundImage from './images/home-background.png'; // 背景画像のパスをインポート
+// import backgroundImage from './images/home-background.png'; // 背景画像のパスをインポート
 
 function Home() {
   const navigate = useNavigate();
@@ -10,22 +10,23 @@ function Home() {
   };
 
   return (
-    <div style={{
-      textAlign: 'center',
-      marginTop: '50px',
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover', // 背景画像をカバーするように設定
-      backgroundPosition: 'center', // 背景画像を中央に配置
-      backgroundRepeat: 'no-repeat', // 画像を繰り返さない
-      width: '100%',
-      height: '90vh' }}>
-
+    // <div style={{
+    //   textAlign: 'center',
+    //   marginTop: '50px',
+    //   backgroundImage: `url(${backgroundImage})`,
+    //   backgroundSize: 'cover', // 背景画像をカバーするように設定
+    //   backgroundPosition: 'center', // 背景画像を中央に配置
+    //   backgroundRepeat: 'no-repeat', // 画像を繰り返さない
+    //   width: '100%',
+    //   height: '90vh' }}>
+    <div>
       <img src="image.png" alt="自分、なに飲むん？" style={{ maxWidth: '100%', marginBottom: '25px' }} />
       <p style={{backgroundColor: 'white'}}>居酒屋やバーで、どのお酒を飲むか悩んでいるそこのあなたへ</p>
       <button onClick={navigateToRoulette}>ルーレットで決める</button>
       <p style={{ position: 'absolute', bottom: '0px', textAlign: 'center', marginLeft: '15px', backgroundColor: 'white' }}>
         ※本アプリはスマホでの使用を推奨しています。
       </p>
+    {/* // </div> */}
     </div>
   );
 }

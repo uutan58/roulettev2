4c48bcb33bcca17344d4b2e8d5f4766dead34b45
uutan <58uutan92@gmail.com>
2024-03-20 17:03:25 +0900

    fix:CSSを調整

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import backgroundImage from './images/home-background.png'; // 背景画像のパスをインポート
+import backgroundImage from './images/favicon.png'; // 背景画像のパスをインポート
 
 function Home() {
   const navigate = useNavigate();
@@ -10,23 +10,45 @@ function Home() {
   };
 
   return (
-    // <div style={{
-    //   textAlign: 'center',
-    //   marginTop: '50px',
-    //   backgroundImage: `url(${backgroundImage})`,
-    //   backgroundSize: 'cover', // 背景画像をカバーするように設定
-    //   backgroundPosition: 'center', // 背景画像を中央に配置
-    //   backgroundRepeat: 'no-repeat', // 画像を繰り返さない
-    //   width: '100%',
-    //   height: '90vh' }}>
+    <div style={{
+      textAlign: 'center',
+      // marginTop: '50px',
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: '100%', // 背景画像をカバーするように設定
+      backgroundPosition: 'center', // 背景画像を中央に配置
+      backgroundRepeat: 'no-repeat', // 画像を繰り返さない
+      width: '100%',
+      height: '100vh' }}>
+    <div style={{
+      backgroundColor: '#fff9d9',
+      }}>
     <div>
       <img src="image.png" alt="自分、なに飲むん？" style={{ maxWidth: '100%', marginBottom: '25px' }} />
-      <p style={{backgroundColor: 'white'}}>居酒屋やバーで、どのお酒を飲むか悩んでいるそこのあなたへ</p>
-      <button onClick={navigateToRoulette}>ルーレットで決める</button>
-      <p style={{ position: 'absolute', bottom: '0px', textAlign: 'center', marginLeft: '15px', backgroundColor: 'white' }}>
+      <p style={{fontFamily: 'Sawarabi Mincho', color: '#071e3e', backgroundColor: '#fff9d9'}}>居酒屋やバーで、どのお酒を飲むか悩んでいるそこのあなたへ</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button onClick={navigateToRoulette}
+          style={{
+            fontFamily: 'Sawarabi Mincho',
+            color: '#071e3e',
+            padding: '10px',
+            margin: '20px',
+            borderRadius: '5px',
+            }}>
+            飲み始める
+        </button>
+      </div>
+      <p style={{
+        fontFamily: 'Sawarabi Mincho',
+        position: 'absolute',
+        bottom: '0px',
+        textAlign: 'center',
+        marginLeft: '15px',
+        backgroundColor: 'white'
+        }}>
         ※本アプリはスマホでの使用を推奨しています。
       </p>
-    {/* // </div> */}
+      </div>
+    </div>
     </div>
   );
 }

532ad37d9831f813718c0513f8b7ff4ed9628fd9
uutan <58uutan92@gmail.com>
2024-03-21 23:02:04 +0900

    fix:ルーレットの文字の縁を消した

--- src/Home.js
+++ src/Home.js
@@ -19,36 +19,36 @@ function Home() {
       backgroundRepeat: 'no-repeat', // 画像を繰り返さない
       width: '100%',
       height: '100vh' }}>
-    <div style={{
-      backgroundColor: '#fff9d9',
-      }}>
-    <div>
-      <img src="image.png" alt="自分、なに飲むん？" style={{ maxWidth: '100%', marginBottom: '25px' }} />
-      <p style={{fontFamily: 'Sawarabi Mincho', color: '#071e3e', backgroundColor: '#fff9d9'}}>居酒屋やバーで、どのお酒を飲むか悩んでいるそこのあなたへ</p>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button onClick={navigateToRoulette}
-          style={{
-            fontFamily: 'Sawarabi Mincho',
-            color: '#071e3e',
-            padding: '10px',
-            margin: '20px',
-            borderRadius: '5px',
-            }}>
-            飲み始める
-        </button>
-      </div>
-      <p style={{
-        fontFamily: 'Sawarabi Mincho',
-        position: 'absolute',
-        bottom: '0px',
-        textAlign: 'center',
-        marginLeft: '15px',
-        backgroundColor: 'white'
+      <div style={{
+        backgroundColor: '#fff9d9',
         }}>
-        ※本アプリはスマホでの使用を推奨しています。
-      </p>
+        <div>
+          <img src="image.png" alt="自分、なに飲むん？" style={{ maxWidth: '100%', marginBottom: '25px' }} />
+          <p style={{fontFamily: 'Sawarabi Mincho', color: '#071e3e', backgroundColor: '#fff9d9'}}>次の1杯は運命に任せてみませんか?「お酒ルーレット」は、居酒屋や外食時に何を飲もうか迷った時の強い味方です。豊富な種類からランダムに1杯が選ばれ、新たなお気に入りになるかもしれません。ワンタップで新しいお酒を発見でき、楽しい驚きを提供します。お酒ルーレットで、新しい飲み物の世界を探検してみてください</p>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <button onClick={navigateToRoulette}
+                style={{
+                  fontFamily: 'Sawarabi Mincho',
+                  color: '#071e3e',
+                  padding: '10px',
+                  margin: '20px',
+                  borderRadius: '5px',
+                  }}>
+                  飲み始める
+              </button>
+            </div>
+          <p style={{
+            fontFamily: 'Sawarabi Mincho',
+            position: 'absolute',
+            bottom: '0px',
+            textAlign: 'center',
+            marginLeft: '15px',
+            backgroundColor: '#fff9d9'
+            }}>
+            ※本アプリはスマホでの使用を推奨しています。
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }

27ea657820580c7377f984a5cbef14dbe0812b45
uutan <58uutan92@gmail.com>
2024-03-26 19:39:11 +0900

    add:Googleアナリティクスを設定

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Roulette from './Roulette';
 import Checkout from './Checkout';
+import { usePageTracking } from './useTracking';
 
 function App() {
+  usePageTracking();
   return (
     <div style={{ backgroundColor: '#fff9d9', minHeight: '100vh' }}>
       <Router>
